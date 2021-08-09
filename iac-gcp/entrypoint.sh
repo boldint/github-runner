04db6d74f24448b71793d05454f1b3bd5596996a
Antonio Castro <antonio.castro@hotmail.com>
2021-08-09 11:45:14 +0100

    Addition of multi-staging docker

--- iac-gcp/entrypoint.sh
+++ iac-gcp/entrypoint.sh
@@ -1,17 +1,26 @@
 #!/usr/bin/env bash
 set -eEuo pipefail
 
-cleanup() {
-  ./config.sh remove --token "${TOKEN}"
-}
+ORGANIZATION=$ORGANIZATION
+REPO=$REPO
+TOKEN=$TOKEN
+NAME=$NAME
+_RUNNER_WORKDIR=$_RUNNER_WORKDIR
 
+echo "Configuring"
 ./config.sh \
-  --url "https://github.com/${OWNER}/${REPO}" \
+  --url "https://github.com/${ORGANIZATION}/${REPO}" \
   --token "${TOKEN}" \
   --name "${NAME}" \
   --unattended \
-  --work _work
+  --work "${_RUNNER_WORKDIR}"
 
+cleanup() {
+  echo "Removing runner ..."
+  ./config.sh remove --unattended --token "${TOKEN}"
+}
+
+echo "Running"
 ./run.sh
 
 cleanup
